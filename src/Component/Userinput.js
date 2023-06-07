8ffff512295195dbbe345b751d3d5488f7231b74
Balsod <balsodconcept@gmail.com>
2023-06-07 14:32:53 +0100

    first commit

---
+++ src/Component/Userinput.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Userinput()  {
+    return (
+    <div>
+  <input type="text" placeholder='Responses' className='input'></input>
+    </div>
+  )
+}
+
+
+export default Userinput
---
+++ src/Component/Useroutput.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Useroutput(props){
+    return (
+        <div className='body'>
+            <p>Making someone feel special isn’t an easy task, 
+            but a beautiful comment on a girl’s</p>
+            <br></br>
+            <br></br>
+            <p>Nowadays, commenting on a girl’s picture has become pretty standard to impress her,
+             but how better and unique it will be for the girl to make your best comment stand out.
+            You know,a comment can be one of the best ways to start a conversation,and a good comment can make your relationship better and your bond stronger.</p>
+
+        </div>
+      )   
+}
+
+
+
+export default Useroutput
